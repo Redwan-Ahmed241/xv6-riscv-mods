ba6538009dfcacc2ceecb706a5b7c7d751df0f6e
Shah Redwan Ahmed <sahmed221241@bscse.uiu.ac.bd>
2025-07-13 11:40:16 +0600

    hmm

--- kernel/elf.h
+++ kernel/elf.h
@@ -1,10 +1,18 @@
 // Format of an ELF executable file
 
-#define ELF_MAGIC 0x464C457FU  // "\x7FELF" in little endian
+#define ELF_MAGIC 0x464C457FU // "\x7FELF" in little endian
+// Values for Proghdr type
+#define ELF_PROG_LOAD 1
+
+// Flag bits for Proghdr flags
+#define ELF_PROG_FLAG_EXEC 1
+#define ELF_PROG_FLAG_WRITE 2
+#define ELF_PROG_FLAG_READ 4
 
 // File header
-struct elfhdr {
-  uint magic;  // must equal ELF_MAGIC
+struct elfhdr
+{
+  uint magic; // must equal ELF_MAGIC
   uchar elf[12];
   ushort type;
   ushort machine;
@@ -22,7 +30,8 @@ struct elfhdr {
 };
 
 // Program section header
-struct proghdr {
+struct proghdr
+{
   uint32 type;
   uint32 flags;
   uint64 off;
@@ -32,11 +41,3 @@ struct proghdr {
   uint64 memsz;
   uint64 align;
 };
-
-// Values for Proghdr type
-#define ELF_PROG_LOAD           1
-
-// Flag bits for Proghdr flags
-#define ELF_PROG_FLAG_EXEC      1
-#define ELF_PROG_FLAG_WRITE     2
-#define ELF_PROG_FLAG_READ      4
